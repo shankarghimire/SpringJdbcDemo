f6fed5150a341f28bde93aeddfcbcd3f2bb79750
Shankar Ghimire <ssghimire@gmail.com>
2020-12-30 02:24:22 -0500

    Adds the Update, Delete and Select operation for Database

--- src/main/java/com/springjdbc/App.java
+++ src/main/java/com/springjdbc/App.java
@@ -1,5 +1,7 @@
 package com.springjdbc;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -22,14 +24,38 @@ public class App
     
     	StudentDao studentDao = applicationContext.getBean("studentDao",StudentDao.class);
     	
-    	Student student = new Student();
-    	student.setName("Harry");
-    	student.setAddress("Toronto");
-    	int result = studentDao.insert(student);
-    	System.out.println("Number of records inserted : " + result);
+    	
+    	//Insert new Record
+//    	Student student = new Student();
+//    	student.setName("Harry");
+//    	student.setAddress("Toronto");
+//    	int result = studentDao.insert(student);
+//    	System.out.println("Number of records inserted : " + result);
+    	
+    	//Update Record
+//    	Student std = new Student();
+//    	std.setId(1);
+//    	std.setName("Samriddha");
+//    	std.setAddress("Nepal");
+//    	int result = studentDao.edit(std);
+//    	System.out.println("Number of Records updated : " + result);
+    	
+    	//Delete Operation
+//    	int studentId = 1;
+//    	int result = studentDao.delete(studentId);
+//    	System.out.println("Number of deleted records : " + result);
     	
     	
+    	//Select operation for single record
+//    	Student student = new Student();
+//    	student = studentDao.getStudent(3);
+//    	System.out.println(student);
     	
+    	//Select operation for all Students
+    	List<Student> listStudents = studentDao.getAllStudents();
+    	for(Student std : listStudents) {
+    		System.out.println(std);
+    	}
     	
     }
 }
--- src/main/java/com/springjdbc/dao/StudentDao.java
+++ src/main/java/com/springjdbc/dao/StudentDao.java
@@ -1,8 +1,15 @@
 package com.springjdbc.dao;
 
+import java.util.List;
+
 import com.springjdbc.entities.Student;
 
 public interface StudentDao {
 	public int insert(Student student);
+	public int edit(Student student);
+	public int delete(int studentId);
+	public Student getStudent(int studentId);//To extract a single row
+	public List<Student>getAllStudents(); //To extract all records
+	
 
 }
